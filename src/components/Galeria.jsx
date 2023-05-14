9d0364058641a2ecfd7783834fd6fd4babc5d4d4
CPinoLeon <cpino012@gmail.com>
2023-05-14 17:23:33 -0400

    toggleHeart function added

--- src/components/Galeria.jsx
+++ src/components/Galeria.jsx
@@ -6,12 +6,19 @@ import { useContext } from "react";
 export default function Home() {
 
   const {fotos, setFotos} = useContext(MyContext)
+  
+  const toggleHeart = (i) => {
+    const index = fotos.findIndex (foto => foto.id == i)
+    fotos[index].liked = !fotos[index].liked
+    setFotos([...fotos])
+    console.log(fotos)
+}
 
   return (
     <div className="galeria grid-columns-5 p-3">
       {fotos.map((foto)=>(
-        <div key= {foto.id} className="galeria foto" style={{backgroundImage: "url(" + foto["src"].medium + ")"}}>
-            <Heart filled={foto.liked}/>
+        <div key= {foto.id} className="galeria foto" style={{backgroundImage: "url(" + foto["src"].medium + ")"}} onClick={()=> toggleHeart(foto.id)}>
+            <Heart filled={foto.liked} />
             <p className="titulo">{foto["alt"]}</p>
         </div>
       ))}
--- src/views/Favoritos.jsx
+++ src/views/Favoritos.jsx
@@ -1,10 +1,14 @@
+import MyContext from "../MyContext";
+import { useContext } from "react";
+
 export default function Favoritos() {
-  
+  const {fotos, setFotos} = useContext(MyContext)
+
     return (
       <div>
         <h1>Fotos favoritas</h1>
         <div className="p-3 galeria grid-columns-4">
-         
+          
         </div>
       </div>
     );
