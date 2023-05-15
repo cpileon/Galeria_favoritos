e9935c696445103ab1e56794a2960536235e4e28
CPinoLeon <cpino012@gmail.com>
2023-05-14 22:13:22 -0400

    Impementing favorites page

--- src/views/Favoritos.jsx
+++ src/views/Favoritos.jsx
@@ -8,7 +8,11 @@ export default function Favoritos() {
       <div>
         <h1>Fotos favoritas</h1>
         <div className="p-3 galeria grid-columns-4">
-          
+          {fotos.filter(f => f.liked == true).map((foto)=>(
+          <div key= {foto.id} className="galeria fav" style={{backgroundImage: "url(" + foto["src"].medium + ")"}} onClick={()=> toggleHeart(foto.id)}>
+              <p className="titulofav">{foto["alt"]}</p>
+          </div>
+          ))}
         </div>
       </div>
     );
